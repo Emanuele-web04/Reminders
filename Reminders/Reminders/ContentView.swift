d4fca59b58e60d6f690ac6f7219890c1c470b23d
Emanuele-web04 <emanueledipietro612@gmail.com>
2023-11-14 17:29:42 +0100

    Start

---
+++ Reminders/Reminders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminders
+//
+//  Created by Emanuele Di Pietro on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminders/Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Emanuele Di Pietro on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
